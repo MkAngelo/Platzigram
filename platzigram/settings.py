617045fb6bbab7b9d71520475e1d1da564eef0cc
MkAngelo <mikequi000@gmail.com>
2021-07-20 20:31:32 -0500

    Listo para produccion

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-27l1eof8ngjo28ewr-0+oeeg03sgv=k-36#4k%g#m4e*$a2y+f'
+SECRET_KEY = os.getenv('PLATZIGRAM_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'platzigram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('PLATZIGRAM_NAME'),
+        'USER': os.getenv('PLATZIGRAM_USER'),
+        'PASSWORD': os.getenv('PLATZIGRAM_PASSWORD'),
+        'HOST': os.getenv('PLATZIGRAM_HOST'),
+        'PORT': os.getenv('PLATZIGRAM_PORT'),
     }
 }
 
